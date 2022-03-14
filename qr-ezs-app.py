5abdbf80774b87dfab8d508ff1b9c380aa789663
MrHans13 <hanspeter.fluetsch@gmx.net>
2022-03-14 21:55:27 +0100

    update 0.1 - bugfixes

--- qr-ezs-app.py
+++ qr-ezs-app.py
@@ -63,8 +63,8 @@ def create_Labels(l_text, r, c, cs, st, f_size=2, py=3):
 
 
 if __name__ == '__main__':
-    label_german = str(pathlib.Path().absolute()) + '/qr-ezs-app/.labels_de.txt'
-    label_english = str(pathlib.Path().absolute()) + '/qr-ezs-app/.labels_en.txt'
+    label_german = str(pathlib.Path().absolute()) + '/.labels_de.txt'
+    label_english = str(pathlib.Path().absolute()) + '/.labels_en.txt'
     bill_path = str(pathlib.Path().absolute())
 
     mainwindow = tk.Tk()
